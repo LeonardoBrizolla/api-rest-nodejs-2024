0ca36d1c89d0ed74e545c3a98d68a42fff681d0e
Leonardo Brizolla <leonardobrizollars@gmail.com>
2024-03-12 19:37:00 -0300

    Testando criação de transação

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,5 @@
-import fastify from 'fastify'
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-import fastifyCookie from '@fastify/cookie'
-
-const app = fastify()
-
-app.register(fastifyCookie)
-
-app.addHook('preHandler', async (request, reply) => {
-  return reply.send(console.log(`[${request.method}] ${request.url}`))
-})
-
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
 
 app
   .listen({
